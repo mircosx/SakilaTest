43071845221f24b79f346858db4ebf01e98323d1
Mirco Castellani <mircosx@gmail.com>
2011-11-22 14:03:47 +0100

    Prueba clase 22 nov

---
+++ src/Util/HibernateUtil.java
@@ -0,0 +1,26 @@
+package Util;
+
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author Mircosx
+ */
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+    
+    static {
+        try {
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
